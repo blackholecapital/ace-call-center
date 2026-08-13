b1cb64c9bcf714bfb31f513f79851a35b389f9eb
factory-admin <info@blackholecapital.xyz>
2026-08-13 14:30:07 -0400

    Build ACE Host AI call center dashboard

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,12 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-if (window.location.pathname.startsWith("/buddy-dashboard")) {
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-} else {
-  window.location.replace("/operator/");
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
